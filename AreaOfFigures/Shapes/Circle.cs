4ddff2c227033e857e8806c0d969eb653f9902e5
Zigguraptor <cobaltum8@gmail.com>
2023-06-20 21:10:45 +0400

    Created Circle.cs

---
+++ AreaOfFigures/Shapes/Circle.cs
@@ -0,0 +1,17 @@
+﻿using AreaOfFigures.Interfaces;
+
+namespace AreaOfFigures.Shapes;
+
+public class Circle : IShape
+{
+    public Circle(float radius)
+    {
+        if (radius <= 0f)
+            throw new ArgumentException("Impossible circle");
+
+        Radius = radius;
+    }
+
+    public float Radius { get; }
+    public float? Area => Radius > 0 ? Radius * Radius * float.Pi : null;
+}
